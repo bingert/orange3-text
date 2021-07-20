c86f617fb6c54b739d290046a5b0baf52b50bda1
juri.gontard <juri.gontard@gwdg.de>
2021-07-20 11:42:13 +0200

    fixed typos

--- orangecontrib/text/widgets/owmine.py
+++ orangecontrib/text/widgets/owmine.py
@@ -16,7 +16,7 @@ from orangecontrib.text.mine import MineAPI
 
 class OWMine(OWWidget):
     """ Get articles from Mine. """
-    name = 'line'
+    name = 'Mine'
     priority = 160
     icon = 'icons/MINE-Logo-orange.svg'
 
